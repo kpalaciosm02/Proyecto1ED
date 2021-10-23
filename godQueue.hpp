f0820366a972b082df046234a1d432890d84af00
kpalacios <kpalaciosm02@gmail.com>
2021-10-22 23:13:59 -0600

    God Queue almost ready.

---
+++ godQueue.hpp
@@ -0,0 +1,94 @@
+#include "godNode.hpp"
+
+#ifndef GQUEUE
+#define GQUEUE
+
+class godQueue{
+    //Priority queue of gods
+    private:
+        godNode * front, * rear;
+    public:
+        godQueue(){
+            /*front = NULL;
+            rear = NULL;
+            front->set_next(rear);*/
+            front = rear = NULL;
+        }
+        bool isEmpty(){
+            //returns true if the queue is empty
+            return front == NULL;
+        }
+        int size(){
+            if (isEmpty())
+                return 0;
+            else{
+                int length = 0;
+                godNode * tmp = front;
+                while (tmp != NULL){
+                    length ++;
+                    tmp = tmp->get_next();
+                }
+                return length;
+            }
+        }
+        void enqueue(godNode * _node){
+            if (isEmpty()){
+                front = rear = _node;
+            }   
+            else{
+                if (front->get_next() == NULL){
+                    if (front->get_god().get_followers() < _node->get_god().get_followers()){
+                        rear = _node;
+                        front->set_next(rear);
+                    }
+                    else{
+                        _node->set_next(front);
+                        front = _node;
+                        rear = front->get_next();    
+                    }
+                }
+                else{
+                    if (front->get_god().get_followers() > _node->get_god().get_followers()){
+                        _node->set_next(front);
+                        front = _node;
+                    }
+                    else if(rear->get_god().get_followers() < _node->get_god().get_followers()){
+                        rear->set_next(_node);
+                        rear = rear->get_next();
+                    }
+                    else{
+                        godNode * tmp = front;
+                        while (tmp->get_next() != NULL){
+                            if (tmp->get_next()->get_god().get_followers() > _node->get_god().get_followers() && tmp->get_god().get_followers() < _node->get_god().get_followers()){
+                                godNode * tmpNext = tmp->get_next();
+                                tmp->set_next(_node);
+                                _node->set_next(tmpNext);
+                                break;
+                            }
+                            else{
+                                tmp = tmp->get_next();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        void print(){
+            if (isEmpty())
+                cout << "Empty queue." << endl;
+            else if(front == rear){
+                front->print();
+                cout << endl;
+            }
+            else{
+                godNode * tmp = front;
+                while (tmp != NULL){
+                    tmp->print();
+                    tmp = tmp->get_next();
+                }
+                cout  << endl;
+            }
+        }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "card.hpp"
 #include "god.hpp"
+#include "godQueue.hpp"
 #include "cardNode.hpp"
 #include "cardStack.hpp"
 
 int main(){
-    Card card1("milagro");
+    /*Card card1("milagro");
     card1.cleanPrint();
     cout << endl;
     cardNode cNode1("traicion");
@@ -21,5 +22,23 @@ int main(){
     cStack->push("prueba3");
     cStack->print();
     cStack->pop();
-    cStack->print();
+    cStack->print();*/
+    godNode * gNode = new godNode("Athenea",170);
+    godNode * gNode2 = new godNode("Zeus", 150);
+    godNode * gNode3 = new godNode("Hades", 200);
+    godNode * gNode4 = new godNode("Loki", 100);
+    godNode * gNode5 = new godNode("Thor", 160);
+    godQueue * gQueue = new godQueue();
+    gQueue->print();
+    gQueue->enqueue(gNode);
+    gQueue->print();
+    gQueue->enqueue(gNode2);
+    gQueue->print();
+    gQueue->enqueue(gNode3);
+    gQueue->print();
+    gQueue->enqueue(gNode4);
+    gQueue->enqueue(gNode5);
+    gQueue->print();
+    God * god = new God(100, "Athenea");
+    //god->clearPrint();
 }
