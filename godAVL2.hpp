5b6fbbadbcd0b4b8fc87c867de839453015afa1e
kpalacios <kpalaciosm02@gmail.com>
2021-10-30 21:05:13 -0600

    milagro y traicion casi listos, revisar si se cambian bien los datos sin errores

--- godAVL2.hpp
+++ godAVL2.hpp
@@ -40,6 +40,20 @@ class avlTree{
 	        return f;
         }
         //methods to rotate nodes in the tree
+        avl_node * rightRotate(avl_node * y){
+            avl_node * x = y->left;
+            avl_node * t2 = x->right;
+            x->right = y;
+            y->left = t2;
+            return x;
+        }
+        avl_node * leftRotate(avl_node * x){
+            avl_node * y = x->right;
+            avl_node * t2 = y->left;
+            y->left = x;
+            x->right = t2;
+            return y;
+        }
         avl_node * rr_rotation(avl_node * parent){//right-right case
             avl_node * tmp = parent->right;
             parent->right = tmp->left;
@@ -88,15 +102,81 @@ class avlTree{
                 root->right = NULL;
                 return root;
             }
-            else if (data <= root->data){
+            else if (data < root->data){
                 root->left = insert(root->left,god);
                 root = balance(root);
             }
-            else if (data > root->data){
+            else if (data >= root->data){
                 root->right = insert(root->right,god);
                 root = balance(root);
             }
             return root;
+        }
+        avl_node * findMin(avl_node * t){
+            if (t == NULL)
+                return NULL;
+            else if (t->left == NULL)
+                return t;
+            else
+                return findMin(t->left);
+        }
+        avl_node * remove(avl_node * root, godNode * god){//searches god by followers and name and deletes it if found.
+            int key = god->get_god().get_followers();//searching node
+            //cout << god->get_god().get_name() << ":" << key << endl;
+            if (root == NULL)
+                return root;
+            else if (key < root->data)
+                root->left = remove(root->left, god);
+            else if (key > root->data)
+                root->right = remove(root->right, god);
+            else if (god->get_god().get_name() != root->god->get_god().get_name()){
+                root->right = remove(root->right, god);
+            }
+            else{//node found
+                if ((root->left == NULL) || root->right == NULL){
+                    avl_node * tmp = root->left ? root->left : root->right;
+                    if (tmp == NULL){
+                        tmp = root;
+                        root = NULL;
+                    }
+                    else{
+                        *root = *tmp;
+                    }
+                    free(tmp);
+                }
+                /*else if((root->left == NULL) && (root->right == NULL)){
+                    root = NULL;
+                }*/
+                else{
+                   avl_node * tmp = findMin(root->right);
+                   root->data = tmp->data;
+                   root->god = tmp->god; 
+                   root->right = remove(root->right, tmp->god);
+                }
+            }
+            if (root == NULL)
+                return NULL;
+            root = balance(root);
+            /*int balanceFactor = diff(root);
+            if (balanceFactor > 1){
+                if (diff(root->left) >= 0 ){
+                    return rightRotate(root);
+                }
+                else{
+                    root->left = leftRotate(root->left);
+                    return rightRotate(root);
+                }
+            }
+            else if (balanceFactor < -1){
+                if (diff(root->right) <= 0){
+                    return leftRotate(root);
+                }
+                else{
+                    root->right = rightRotate(root->right);
+                    return leftRotate(root);
+                }
+            }*/
+            return root; 
         }
         void printAVL(avl_node * root){
             if (root != NULL){
@@ -106,7 +186,16 @@ class avlTree{
                 printAVL(root->left);
                 cout << "]";
             }
-        }    
+        }
+        void preOrder(avl_node * root){
+            if (root != NULL){
+                cout << root->god->get_god().get_name() << ":" << root->god->get_god().get_followers() << "[";
+                preOrder(root->left);
+                cout << "][";
+                preOrder(root->right);
+                cout << "]";
+            }
+        } 
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ avlTree llenarAVL(avlTree avl, godQueue * queue){
     godNode * tmp = queue->first();
     while (tmp != NULL){
         avl.root = avl.insert(avl.root,tmp);
+        //avl.preOrder(avl.root);
+        //cout << endl;
         tmp = tmp->get_next();
     }
     return avl;
@@ -23,17 +25,47 @@ avlTree milagro(avlTree avl, godQueue * queue){
     godNode * a = queue->dequeue();
     godNode * b = queue->dequeue();
     int extra20 = b->get_god().get_followers()*0.2;
+    avl.root = avl.remove(avl.root,a);
+    avl.root = avl.remove(avl.root,b);
     God gb = b->get_god();
     God ga = a->get_god();
-    gb.set_followers(b->get_god().get_followers()-extra20);
-    ga.set_followers(a->get_god().get_followers()+extra20);
+    gb.set_followers(gb.get_followers()-extra20);
+    ga.set_followers(ga.get_followers()+extra20);
     a->set_god(ga);
     b->set_god(gb);
     queue->enqueue(a);
     queue->enqueue(b);
-    avlTree newAVL;
-    newAVL = llenarAVL(newAVL,queue);
-    return newAVL;
+    avl.root = avl.insert(avl.root,a);
+    avl.root = avl.insert(avl.root,b);
+    return avl;
+}
+
+avlTree traicion(avlTree avl, godQueue * queue){
+    //realiza la accion de la carta traicion
+    godNode * a = queue->dequeue();
+    godNode * b = queue->dequeue();
+    int extra30 = a->get_god().get_followers()*0.3;
+    avl.root = avl.remove(avl.root,a);
+    avl.root = avl.remove(avl.root,b);
+    God ga = a->get_god();
+    God gb = b->get_god();
+    ga.set_followers(ga.get_followers()-extra30);
+    gb.set_followers(gb.get_followers()+extra30);
+    a->set_god(ga);
+    b->set_god(gb);
+    queue->enqueue(a);
+    queue->enqueue(b);
+    avl.root = avl.insert(avl.root,a);
+    avl.root = avl.insert(avl.root,b);
+    return avl;
+}
+
+avl_node * anarquia(avlTree avl, godQueue * queue){
+    godNode * a = queue->dequeue();
+    int key = a->get_god().get_followers();
+    avl_node * tmp = avl.root;
+    if (tmp == NULL)
+        return NULL;
 }
 
 int main(){
@@ -60,6 +92,15 @@ int main(){
     godNode * gNode3 = new godNode("Hades", 200);
     godNode * gNode4 = new godNode("Loki", 160);
     godNode * gNode5 = new godNode("Thor", 100);
+    godNode * gNode6 = new godNode("Ra", 124);
+    godNode * gNode7 = new godNode("Isis", 156);
+    godNode * gNode8 = new godNode("Afrodita", 181);
+    godNode * gNode9 = new godNode("Apollo", 291);
+    godNode * gNode10 = new godNode("Ares", 111);
+    godNode * gNode11 = new godNode("Artemisa", 165);
+    godNode * gNode12 = new godNode("Apollo", 154);
+    godNode * gNode13 = new godNode("Poseidon", 119);
+    godNode * gNode14 = new godNode("Hercules", 302);
     godQueue * gQueue = new godQueue();
     //gQueue->print();
     gQueue->enqueue(gNode);
@@ -70,14 +111,49 @@ int main(){
     //gQueue->print();
     gQueue->enqueue(gNode4);
     gQueue->enqueue(gNode5);
+    gQueue->enqueue(gNode6);
+    gQueue->enqueue(gNode7);
+    gQueue->enqueue(gNode8);
+    gQueue->enqueue(gNode9);
+    gQueue->enqueue(gNode10);
+    gQueue->enqueue(gNode11);
+    gQueue->enqueue(gNode12);
+    gQueue->enqueue(gNode13);
+    gQueue->enqueue(gNode14);
     gQueue->print();
     //arbol AVL
     avlTree avl;
     avl = llenarAVL(avl,gQueue);
-    avl.printAVL(avl.root);
-    cout << endl;
+    avl.printAVL(avl.root); 
+    cout << endl << "Milagro1" << endl;  
     avl = milagro(avl,gQueue);
     avl.printAVL(avl.root);
-    cout << endl;
-    gQueue->print();    
+    cout << endl << "Traicion1" << endl;
+    gQueue->print();
+    avl = traicion(avl,gQueue);
+    avl.printAVL(avl.root);
+    /*cout << endl << "Milagro2" << endl;
+    avl = milagro(avl,gQueue);
+    avl.printAVL(avl.root);
+    cout << endl << "Traicion2" << endl;
+    gQueue->print();
+    avl = traicion(avl,gQueue);
+    avl.printAVL(avl.root);
+    cout << endl << "Milagro3" << endl;
+    avl = milagro(avl,gQueue);
+    avl.printAVL(avl.root);
+    cout << endl << "Traicion3" << endl;
+    gQueue->print();
+    avl = traicion(avl,gQueue);
+    avl.printAVL(avl.root);
+    cout << endl << "Milagro4" << endl;
+    avl = milagro(avl,gQueue);
+    avl.printAVL(avl.root);
+    cout << endl << "Traicion4" << endl;
+    gQueue->print();
+    avl = traicion(avl,gQueue);
+    avl.printAVL(avl.root);*/
+//    avl.remove(avl.root,gNode3);
+//    avl.remove(avl.root,gNode2);
+//    avl.printAVL(avl.root);
 }
